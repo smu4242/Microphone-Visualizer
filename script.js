0648d46e2cab7cb6e8f3b79045b78f1b3f2daebc
smu4242 <smu4242@users.noreply.github.com>
2020-01-07 17:41:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ window.onload = function () {
         //significant data after this point. Not worth visualizing.
         for (var i = 0 ; i < 255; i++) {
             path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-	        line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-	        //<line x1="100%" y1="0%" x2="0%" y2="100%" style="stroke: rgb(234, 243, 234);stroke-width: 5;"></line>
+	    line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+	    //<line x1="100%" y1="0%" x2="0%" y2="100%" style="stroke: rgb(234, 243, 234);stroke-width: 5;"></line>
 
             path.setAttribute('stroke-dasharray', '4,1');
             mask.appendChild(path);
@@ -43,10 +43,10 @@ window.onload = function () {
               	adjustedLength = Math.floor(frequencyArray[i]) - (Math.floor(frequencyArray[i]) % 5);
                 paths[i].setAttribute('d', 'M '+ (i) +',255 l 0,-' + adjustedLength);
                 
-                line[i].setAttribute('x1', ''+ (i) +'%');
-                line[i].setAttribute('y1', '255');
-                line[i].setAttribute('x2', '0');
-                line[i].setAttribute('y2', '-'+ (adjustedLength) +'%');
+//                 line[i].setAttribute('x1', ''+ (i) +'%');
+//                 line[i].setAttribute('y1', '255');
+//                 line[i].setAttribute('x2', '0');
+//                 line[i].setAttribute('y2', '-'+ (adjustedLength) +'%');
             }
 
         }
