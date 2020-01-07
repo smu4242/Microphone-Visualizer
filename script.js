c3cab51581de9a1f8e1f09d3d7f1a6011c1c2ba1
smu4242 <smu4242@users.noreply.github.com>
2020-01-07 17:36:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.onload = function () {
     var mask = visualizer.getElementById('mask');
     var h = document.getElementsByTagName('h1')[0];
     var path;
+    var line;
     var report = 0;
     
     var soundAllowed = function (stream) {
@@ -23,12 +24,16 @@ window.onload = function () {
         var frequencyArray = new Uint8Array(analyser.frequencyBinCount);
         //visualizer.setAttribute('viewBox', '0 0 255 255');
       
-				//Through the frequencyArray has a length longer than 255, there seems to be no
+        //Through the frequencyArray has a length longer than 255, there seems to be no
         //significant data after this point. Not worth visualizing.
         for (var i = 0 ; i < 255; i++) {
             path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+	        line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+	        //<line x1="100%" y1="0%" x2="0%" y2="100%" style="stroke: rgb(234, 243, 234);stroke-width: 5;"></line>
+
             path.setAttribute('stroke-dasharray', '4,1');
             mask.appendChild(path);
+            mask.appendChild(line);
         }
         var doDraw = function () {
             requestAnimationFrame(doDraw);
@@ -37,6 +42,11 @@ window.onload = function () {
             for (var i = 0 ; i < 255; i++) {
               	adjustedLength = Math.floor(frequencyArray[i]) - (Math.floor(frequencyArray[i]) % 5);
                 paths[i].setAttribute('d', 'M '+ (i) +',255 l 0,-' + adjustedLength);
+                
+                line[i].setAttribute('x1', ''+ (i) +'%');
+                line[i].setAttribute('y1', '255');
+                line[i].setAttribute('x2', '0');
+                line[i].setAttribute('y2', '-'+ (adjustedLength) +'%');
             }
 
         }
